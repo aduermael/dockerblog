4d0b57fec55bebcd1761eb1a77aa8c04d1d9fb2d
Gaetan de Villele <gdevillele@gmail.com>
2014-05-31 20:23:38 -0700

    create the dockerfile for the [volumes] container

---
+++ volume/README.md
@@ -0,0 +1,7 @@
+### Building
+
+docker build --no-cache --rm=true -t blog_volumes .
+
+### Running
+
+docker run -t -i -d --name volumes blog_volumes
---
+++ volume/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# START SCRIPT
+#
+# we use a script to avoid the container to exit
+# right after being started
+
+# we keep an interactive TTY
+/bin/bash
